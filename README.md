cbf516ed2d489c0923682b9ba5a7dae63b052a1e
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2016-12-15 15:40:07 +0900

    Turtle and RDF/XML formats are supported.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-Converts a file in RDF/XML to it in N-Triples.
+Converts a file in RDF/XML or Turtle to it in N-Triples.
 
 ConvRDF converts a file in a streaming fashion, so that it can handle huge numbers of triples (> 1 billion) without consuming a huge amount of memory.
 
-This tool requires Apache Jena. We tested it on the version 2.11.0.
+This tool requires Apache Jena. We tested it on the version 2.13.0.
 
 This tool can be used as follows:
 
-$ java -cp ./apache-jena-2.11.0/lib/*:./ConvRDF.jar jp.ac.rois.dbcls.ConvRDF &lt;file to be converted&gt;
+$ java -cp ./apache-jena-2.13.0/lib/*:./ConvRDF.jar jp.ac.rois.dbcls.ConvRDF &lt;file to be converted&gt;
+
+If you want to specify a format, use the -i:&lt;format&gt; option, where &lt;format&gt; is either rdfxml or turtle.
--- src/jp/ac/rois/dbcls/ConvRDF.java
+++ src/jp/ac/rois/dbcls/ConvRDF.java
@@ -10,9 +10,12 @@
 package jp.ac.rois.dbcls;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.riot.RDFLanguages;
@@ -28,56 +31,80 @@ import com.hp.hpl.jena.graph.Triple;
 public class ConvRDF {
 
 	public static void main(String[] args) {
+
+		final Map<String, Lang> optmap = new HashMap<String, Lang>() {
+			{put("turtle", RDFLanguages.TURTLE);}
+            {put("rdfxml", RDFLanguages.RDFXML);}
+        };
+
 		final int interval = 10000;
 		final int buffersize = 100000;
+		String informat = "rdfxml";
+		int idx = 0;
 		if(args.length == 0){
 			System.out.println("Please specify the filename to be converted.");
 			return;
 		} else {
-			File file = new File(args[0]);
+			if(args[idx].startsWith("-i:")){
+				informat = args[idx].substring(3);
+				idx++;
+			}
+			File file = new File(args[idx]);
 			if(!file.exists() || !file.canRead()){
 				System.out.println("Can't read " + file);
 				return;
 			}
 		}
-		final String filename = args[0];
+		if(!optmap.containsKey(informat)){
+			System.out.println("Input format is either turtle or rdfxml.");
+			return;
+		}
+		final String filename = args[idx];
+		final Lang inputformat = optmap.get(informat);
 
-        PipedRDFIterator<Triple> iter = new PipedRDFIterator<Triple>(buffersize);
-        final PipedRDFStream<Triple> inputStream = new PipedTriplesStream(iter);
+		PipedRDFIterator<Triple> iter = new PipedRDFIterator<Triple>(buffersize);
+		final PipedRDFStream<Triple> inputStream = new PipedTriplesStream(iter);
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+		ExecutorService executor = Executors.newSingleThreadExecutor();
 
-        Runnable parser = new Runnable() {
+		Runnable parser = new Runnable() {
 
-            @Override    
-            public void run() {
-            	try{
-            		RDFDataMgr.parse(inputStream, filename, "file:///", RDFLanguages.RDFXML, null);
-            	}
-            	catch (RiotNotFoundException e){
-            		System.err.println("File format error.");
-            	}
-            }
-        };
+			@Override
+			public void run() {
+				try{
+					RDFDataMgr.parse(inputStream, filename, "file:///", inputformat, null);
+					/*
+					if(inputformat.equals("turtle")){
+						RDFDataMgr.parse(inputStream, filename, "file:///", RDFLanguages.TURTLE, null);
+					} else if(inputformat.equals("rdfxml")){
+						RDFDataMgr.parse(inputStream, filename, "file:///", RDFLanguages.RDFXML, null);
+					}
+					*/
+				}
+				catch (RiotNotFoundException e){
+					System.err.println("File format error.");
+				}
+			}
+		};
 
-        executor.submit(parser);
+		executor.submit(parser);
 
-        int i = 0;
-        Graph g = Factory.createDefaultGraph();
-        while (iter.hasNext()) {
-        	Triple next = iter.next();
-        	g.add(next);
-        	i++;
-        	if(i % interval == 0){
-        		RDFDataMgr.write(System.out, g, RDFFormat.NTRIPLES_UTF8);
-        		g = Factory.createDefaultGraph();
-        	}
-        }
-        if(i % interval > 0){
-        	RDFDataMgr.write(System.out, g, RDFFormat.NTRIPLES_UTF8);
-        }
+		int i = 0;
+		Graph g = Factory.createDefaultGraph();
+		while (iter.hasNext()) {
+			Triple next = iter.next();
+			g.add(next);
+			i++;
+			if(i % interval == 0){
+				RDFDataMgr.write(System.out, g, RDFFormat.NTRIPLES_UTF8);
+				g = Factory.createDefaultGraph();
+			}
+		}
+		if(i % interval > 0){
+			RDFDataMgr.write(System.out, g, RDFFormat.NTRIPLES_UTF8);
+		}
 
-        executor.shutdown();
-    }
+		executor.shutdown();
+	}
 
 }
