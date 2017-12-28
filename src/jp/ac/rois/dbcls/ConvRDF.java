f5098a281307da5969791d96332d50f419da97ed
Yasunori <yy@dbcls.rois.ac.jp>
2017-12-28 23:02:24 +0900

    エラーメッセージの改善。

--- src/jp/ac/rois/dbcls/ConvRDF.java
+++ src/jp/ac/rois/dbcls/ConvRDF.java
@@ -56,11 +56,18 @@ public class ConvRDF {
 					parser_object.parse(inputStream);
 				}
 				catch (RiotParseException e){
-					System.err.println("Parse error [" + filename + "]: " + e.getMessage());
+					System.err.println("Parse error at the line: "
+							+ e.getLine()
+							+ " and the column: "
+							+ e.getCol()
+							+ " in \""
+							+ filename
+							+ "\", and cannot parse this file anymore. Reason:"
+							+ e.getMessage() + "(" + e.getOriginalMessage() +")");
 					inputStream.finish();
 				}
 				catch (RiotNotFoundException e){
-					System.err.println("File format error [" + filename + "]: " + e.getMessage());
+					System.err.println("Format error for the file \"" + filename + "\": " + e.getMessage());
 					inputStream.finish();
 				}
 			}
